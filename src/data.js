e95d9bf2dd16c52da019ec224d45b624e4a905b6
MailaFAP <maila_3@hotmail.com>
2023-05-31 09:09:41 -0300

    print da imagem e função para esconder o aside e aparecer quando clicar no botão pesquisar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+import breakingbad from "./data/breakingbad/breakingbad";
+
+
+/*const info = new Array(breakingbad.breaking_bad);
+let nomes = info.filter(i => i === name);
+console.log(nome);
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
--- src/main.js
+++ src/main.js
@@ -1,7 +1,40 @@
-import { example } from "./data.js";
-// import data from './data/lol/lol.js';
-import data from "./data/pokemon/pokemon.js";
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+function imagens() {
+    for(const i in data.breaking_bad) {
+      console.log(data.breaking_bad[i].img);
+    }
+  }
+  imagens();
+  const btnShow = document.querySelector(".botao");
+  const listHidden = document.querySelector(".aside");
+  listHidden.style.display = "none"; // o estado inicial vai ser com o elemento escondido
+  btnShow.addEventListener("click", () => {
+    if (listHidden.style.display === "none") {
+      //com display none o elemento é escondido e com display como block o elemento será exibido
+      listHidden.style.display = "block";
+    } else {
+      listHidden.style.display = "none";
+    }
+  });
+  
+/*document.getElementById("filtro-nome").addEventListener("click", )
+document.getElementById("botao").addEventListener("click", filtrar);
+function filtrar (){
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
