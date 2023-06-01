389a149fae1f0b9d8918b27a7c72415203bfa58a
Diulianne Oliveira <diulicarine@gmail.com>
2023-06-01 10:48:57 -0400

    consegui mostrar na tela as imagens e estilizei o espaço onde elas vão ficar. No html, criei uma div que sinaliza o espaço onde essas imagens vão ficar

--- src/main.js
+++ src/main.js
@@ -1,40 +1,27 @@
 import data from "./data/breakingbad/breakingbad.js";
 
-function imagens() {
-    for(const i in data.breaking_bad) {
-      console.log(data.breaking_bad[i].img);
-    }
+const imgs = document.getElementById("imgs-footer");
+const dados = data.breaking_bad;
+const btnShow = document.querySelector(".botao");
+const listHidden = document.querySelector(".aside");
+
+function imagens(breaking_bad) {
+  imgs.innerHTML = breaking_bad
+    .map(
+      (breaking_bad) =>
+        `<img src="${breaking_bad.img}" class="imgs-personagens" alt="imagens dos personagens">`
+    )
+    .join("");
+}
+imagens(dados);
+
+
+listHidden.style.display = "none"; // o estado inicial vai ser com o elemento escondido
+btnShow.addEventListener("click", () => {
+  if (listHidden.style.display === "none") {
+    //com display none o elemento é escondido e com display como block o elemento será exibido
+    listHidden.style.display = "block";
+  } else {
+    listHidden.style.display = "none";
   }
-  imagens();
-  const btnShow = document.querySelector(".botao");
-  const listHidden = document.querySelector(".aside");
-  listHidden.style.display = "none"; // o estado inicial vai ser com o elemento escondido
-  btnShow.addEventListener("click", () => {
-    if (listHidden.style.display === "none") {
-      //com display none o elemento é escondido e com display como block o elemento será exibido
-      listHidden.style.display = "block";
-    } else {
-      listHidden.style.display = "none";
-    }
-  });
-  
-/*document.getElementById("filtro-nome").addEventListener("click", )
-document.getElementById("botao").addEventListener("click", filtrar);
-function filtrar (){
-
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
