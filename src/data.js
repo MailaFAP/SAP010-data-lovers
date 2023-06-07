8f2de818935b8b3b82623a02d263880a0e6c572a
Diulianne Oliveira <diulicarine@gmail.com>
2023-06-07 11:50:58 -0400

    Fiz a parte do calculo agregado na categoria breaking bad. Fiz o alinhamento do menu e melhorei a parte de responsividade

--- src/data.js
+++ src/data.js
@@ -1,24 +1,27 @@
-
-
 function filtrarCategorias(dados, categoria) {
-  return dados.filter(i => i.category === categoria);
+  return dados.filter((i) => i.category === categoria);
 }
 
 function filtrarStatus(dados, status) {
-  return dados.filter(i => i.status === status);
+  return dados.filter((i) => i.status === status);
 }
 
 function ordenar(dadosFiltrados, abobrinha) {
-  if(abobrinha === "A-Z") {
-    dadosFiltrados.sort((a, b) => a.name > b.name ? 1 : -1);
-  } else if(abobrinha === "Z-A") {
-    dadosFiltrados.sort((a, b) => a.name < b.name ? 1 : -1);
+  if (abobrinha === "A-Z") {
+    dadosFiltrados.sort((a, b) => (a.name > b.name ? 1 : -1));
+  } else if (abobrinha === "Z-A") {
+    dadosFiltrados.sort((a, b) => (a.name < b.name ? 1 : -1));
   }
   return dadosFiltrados;
 }
 
+function calculoAgregado(quantidade, total) {
+  return quantidade / total * 100;
+}
+
 export default {
-  filtrarCategorias: filtrarCategorias, 
-  filtrarStatus: filtrarStatus,
-  ordenar: ordenar
+  filtrarCategorias,
+  filtrarStatus,
+  ordenar,
+  calculoAgregado,
 };
