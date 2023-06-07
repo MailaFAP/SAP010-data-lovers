dedf8980d01894aa734ce310c79c3d00f4342e0d
Diulianne Oliveira <diulicarine@gmail.com>
2023-06-07 12:01:07 -0400

    modifiquei um erro de indentação no data.js

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 function filtrarCategorias(dados, categoria) {
-  return dados.filter((i) => i.category === categoria);
+  return dados.filter(i => i.category === categoria);
 }
 
 function filtrarStatus(dados, status) {
-  return dados.filter((i) => i.status === status);
+  return dados.filter(i => i.status === status);
 }
 
 function ordenar(dadosFiltrados, abobrinha) {
   if (abobrinha === "A-Z") {
-    dadosFiltrados.sort((a, b) => (a.name > b.name ? 1 : -1));
+    dadosFiltrados.sort((a, b) => a.name > b.name ? 1 : -1);
   } else if (abobrinha === "Z-A") {
-    dadosFiltrados.sort((a, b) => (a.name < b.name ? 1 : -1));
+    dadosFiltrados.sort((a, b) => a.name < b.name ? 1 : -1);
   }
   return dadosFiltrados;
 }
